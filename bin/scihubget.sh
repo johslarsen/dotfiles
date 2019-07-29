0ab182a3cc8bf24179f17d93a542d24c89641df3
Johannes Larsen <mail@johslarsen.net>
2019-07-29 18:44:36 +0200

    Fix scihubget.sh
    Now I started getting 503 on downloads, the scihub example used
    --content-disposition, so I added it and now I get access.

--- bin/scihubget.sh
+++ bin/scihubget.sh
@@ -6,11 +6,11 @@ username=johslarsen
 password=$(printf "%s" "$(pass website/scihub.copernicus.eu | head -1)")
 
 for uuid in "$@"; do
-	if wget -O $uuid.zip --no-verbose --show-progress --no-check-certificate --continue --user=$username --password=$password "https://scihub.copernicus.eu/dhus/odata/v1/Products('$uuid')/\$value"; then
+	if wget  --content-disposition -O $uuid.zip --no-verbose --show-progress --no-check-certificate --continue --user=$username --password=$password "https://scihub.copernicus.eu/dhus/odata/v1/Products('$uuid')/\$value"; then
 		name=$(unzip -l $uuid.zip | head -4 | tail -1 | awk '{print $4}' | cut -d '.' -f 1)
 		[ -e $name.zip ] || ln -s $uuid.zip $name.zip
 
-		wget -O - --no-verbose --show-progress --no-check-certificate --user=$username --password=$password "https://scihub.copernicus.eu/dhus/odata/v1/Products('$uuid')/Checksum/Value/\$value" | tr 'A-Z' 'a-z' > $uuid.md5
+		wget  --content-disposition -O - --no-verbose --show-progress --no-check-certificate --user=$username --password=$password "https://scihub.copernicus.eu/dhus/odata/v1/Products('$uuid')/Checksum/Value/\$value" | tr 'A-Z' 'a-z' > $uuid.md5
 		[ -e $name.md5 ] || ln -s $uuid.md5 $name.md5
 
 		md5expected="$(cat $uuid.md5)"
