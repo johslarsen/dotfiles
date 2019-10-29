22546aab5d7365ebc224e0ea088839061a5ff1cc
Johannes Larsen <mail@johslarsen.net>
2019-10-29 19:22:29 +0100

    Add attr customization to extractHrefs.rb

--- bin/extractHrefs.rb
+++ bin/extractHrefs.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
+require 'optparse'
+
+OptionParser.new do |o|
+  o.on("-a", "--attr NAME[,...]", Array, "Extract urls from these attrs (defaults to href,src)")
+end.permute!(into: $opts||={})
 
 content = ARGF.read
 tags = content.split(">")
 
-urls = tags.grep(/href[[:space:]]*=[[:space:]]*(["'])((?:\\\1|.)*?)\1/) {$2}
-puts urls.sort.uniq
+urls = $opts.fetch(:attr, ["href", "src"]).map do |attr|
+  tags.grep(/#{attr}[[:space:]]*=[[:space:]]*(["'])((?:\\\1|.)*?)\1/) {$2}
+end.compact.sort.uniq
+puts urls
