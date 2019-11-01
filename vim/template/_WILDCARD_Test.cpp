0445999852681fd1e9318707b61db095df82266e
Johannes Larsen <mail@johslarsen.net>
2019-11-01 01:32:19 +0100

    Update some C++ vim templates

--- vim/template/_WILDCARD_Test.cpp
+++ vim/template/_WILDCARD_Test.cpp
@@ -1,24 +1,10 @@
-:silent %s/#CLASS#/\=substitute(substitute(substitute(RPath("%:r"), "^test\/", "", "g"), "+", "", "g"), "\/", "_", "g")/g
-:silent %s/#LOGID#/\=substitute(RPath("%:r"), "\/", ".", "g")/g
-:silent %s/#HEADER#/\=RPath("%:t:r").".hpp"/g
-:silent %s/#SRCHEADER#/\=substitute(RPath("%:r"), "^test\/\\(.*\\)Test$", "\\1", "g").".hpp"/g
+:silent %s/#CLASS#/\=substitute(substitute(substitute(RPath("%:r"), "^\\(test\/\\)\\?\\(.*\\)Test$", "\\2", "g"), "+", "", "g"), "\/", "_", "g")/g
+:silent %s/#SRCHEADER#/\=substitute(RPath("%:r"), "^\\(test\/\\)\\?\\(.*\\)Test$", "\\2", "g").".hpp"/g
 :silent %s/#SRCNAMESPACE#/\=NamespaceUsing(substitute(RPath("%:h"), "^test\/", "", "g"))/g
-#include <#SRCHEADER#>
-#include "#HEADER#"
-
-MEOS_SET_TEST_PATH;
+#include <gtest/gtest.h>
+#include "#SRCHEADER#"
 
 #SRCNAMESPACE#
-static auto logger = log4cxx::Logger::getLogger("#LOGID#");
+TEST(#CLASS#, #CURSOR#) {
 
-#CLASS#::#CLASS#()
-{
-}
-
-#CLASS#::~#CLASS#()
-{
-}
-
-TEST_F(#CLASS#, #CURSOR#)
-{
 }
