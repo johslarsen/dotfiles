80afe726fd649ad0541c5dc2f00294b6492f26ac
Johannes Larsen <mail@johslarsen.net>
2022-08-25 02:38:09 +0200

    Add frequency to wifi_status.sh

--- bin/wifi_status.sh
+++ bin/wifi_status.sh
@@ -5,9 +5,11 @@ link="$(iw dev "$dev" link)"
 RE_SSID="SSID: ([^[:space:]]*)"
 RE_SIGNAL="signal: ([-[:digit:]]*)"
 RE_BITRATE="tx bitrate: ([[:digit:]]*)"
+RE_FREQ="freq: ([[:digit:]]*)"
 [[ "$link" =~ $RE_SSID ]] && essid="${BASH_REMATCH[1]}"
 [[ "$link" =~ $RE_SIGNAL ]] && signal="${BASH_REMATCH[1]}"
 [[ "$link" =~ $RE_BITRATE ]] && bitrate="${BASH_REMATCH[1]}"
+[[ "$link" =~ $RE_FREQ ]] && freq="${BASH_REMATCH[1]}"
 
 rfkill list | awk -v RS='^[^\\s]' '{gsub(/\n/, " "); if (NF > 0) print}' | grep -v 'yes'&> /dev/null || flight="<fn=1>✈</fn>"
-[[ "$essid" ]] && printf "%s<fc=#4444ff>%03d</fc>\n" "$essid" "$signal" || echo $flight
+[[ "$essid" ]] && printf "%s<fc=#444488>%s</fc><fc=#4444ff>%03d</fc>\n" "$essid" "${freq:0:1}.${freq:1:2}" "$signal" || echo $flight
