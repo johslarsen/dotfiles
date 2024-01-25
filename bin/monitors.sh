9638c69be210b864de0a1e9c567f5c48f1cfa03e
Johannes Larsen <mail@johslarsen.net>
2024-01-25 21:09:03 +0100

    Replace dstat with dool after upstream rename

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -2,14 +2,14 @@
 
 declare -A monitors
 disks=
-monitors[dstat]="while true; do disks=; DSTAT_TIMEFMT=%H:%M:%S dstat -f -tlcmn --socket -d -D \"\$(\\ls -1 /dev/nvme?n? /dev/[hs]d? /dev/mmcblk? 2>/dev/null | tr $'\n' ,)\" --fs --lock -pygi 10 3; done"
+monitors[dool]="while true; do disks=; DOOL_TIMEFMT=%H:%M:%S dool -f -tlcmn --socket -d -D \"\$(\\ls -1 /dev/nvme?n? /dev/[hs]d? /dev/mmcblk? 2>/dev/null | tr $'\n' ,)\" --fs --lock -pygi 10 3; done"
 monitors[iotop]="sudo iotop"
 monitors[iftop]="sudo iftop"
 monitors[htop]="htop"
 monitors[journal]="journalctl -af | ccze -A"
 
 if ! tmux list-sessions 2>/dev/null | grep '^monitor:' &>/dev/null; then
-    tmux new -x$(tput cols) -y$(tput lines) -d -n main -s monitor "${monitors[dstat]}"
+    tmux new -x$(tput cols) -y$(tput lines) -d -n main -s monitor "${monitors[dool]}"
     tmux select-pane -t monitor
     tmux split -vp 90 "${monitors[journal]}"
     tmux split -vp 75 "${monitors[htop]}"
