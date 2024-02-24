51d1a1e4c357b6f4e35f05b2d647b0fc7916ea1a
Johannes Larsen <mail@johslarsen.net>
2024-02-24 15:14:21 +0100

    Better nvim test integration
    * Support doctest filters
    * Run test associated with the given source file
    * Look harder to find the associated test/source file

--- config/nvim/lua/test.lua
+++ config/nvim/lua/test.lua
@@ -1,15 +1,31 @@
+local function endswith(str, suffix)
+  return str:sub(- #suffix) == suffix
+end
+
+local function relpath(path)
+  return vim.fn.fnamemodify(path, ':p:~:.')
+end
+
 local function src_or_test_filename()
-  local org = vim.api.nvim_buf_get_name(0)
-  local src = org:gsub("_?[Tt]est", "")
+  local org = relpath(vim.api.nvim_buf_get_name(0))
+  local src = org:gsub("_?[Tt]est/?", "")
   if (org ~= src) then
     if vim.fn.filereadable(src) == 1 then return src end
     for _, candidate in pairs(vim.fn.glob(src:gsub("[.][^.]*$", ".*"), false, true)) do
       return candidate
     end
-    return vim.fn.filereadable(src) == 1 and src or nil
+    for _, candidate in pairs(vim.fn.glob("*/" .. src:gsub("[.][^.]*$", ".*"), false, true)) do
+      return candidate
+    end
   else
     for _, pattern in pairs({ "_test%0", "Test%0", "_test*", "Test*" }) do
       for _, candidate in pairs(vim.fn.glob(org:gsub("[.][^.]*$", pattern), false, true)) do
+        return candidate
+      end
+      for _, candidate in pairs(vim.fn.glob("test/" .. org:gsub("[.][^.]*$", pattern), false, true)) do
+        return candidate
+      end
+      for _, candidate in pairs(vim.fn.glob("test/" .. org:gsub("^[^/]*/", ""):gsub("[.][^.]*$", pattern), false, true)) do
         return candidate
       end
     end
@@ -25,15 +41,47 @@ vim.keymap.set('n', '<Leader>T', function() edit_non_nil(src_or_test_filename(),
 vim.keymap.set('n', '<Leader><C-t>', function() edit_non_nil(src_or_test_filename(), vim.cmd.split) end)
 
 
+local function run_in_testbuf(fun)
+  if vim.api.nvim_buf_get_name(0):match("[Tt]est") == nil then
+    local test = src_or_test_filename()
+    if test ~= nil then
+      for _, bufid in ipairs(vim.api.nvim_list_bufs()) do
+        if endswith(vim.api.nvim_buf_get_name(bufid), test) then
+          return vim.api.nvim_buf_call(bufid, fun)
+        end
+      end
+    end
+  end
+  return fun()
+end
+
+local function doctest_suite()
+  local pattern = 'TEST_SUITE[^(]*("\\([^"]*\\)"'
+  local pos = vim.fn.search(pattern, 'bcnw')
+  if (pos == 0) then return '*' end -- TODO: guess filename
+  return vim.fn.trim(vim.fn.matchlist(vim.fn.join(vim.fn.getline(pos, pos+2)), pattern)[2])
+end
+local function doctest_case()
+  local pattern = 'TEST_CASE[^(]*("\\([^"]*\\)"'
+  local pos = vim.fn.search(pattern, 'bcnw')
+  if (pos == 0) then return '*' end
+  return vim.fn.trim(vim.fn.matchlist(vim.fn.join(vim.fn.getline(pos, pos+2)), pattern)[2])
+end
+local function doctest_context()
+  return doctest_suite(), doctest_case()
+end
+
 local function gtest_context()
   local id = '[[:alnum:]_[:space:]\n]'
   local pattern = 'TEST' .. id .. '*(\\(' .. id .. '\\+\\),\\(' .. id .. '\\+\\))'
 
-  local pos = vim.fn.search(pattern, 'bcnw')
-  if pos == 0 then return {'*', '*'} end
+  return run_in_testbuf(function()
+    local pos = vim.fn.search(pattern, 'bcnw')
+    if pos == 0 then return doctest_context() end
 
-  local match = vim.fn.matchlist(vim.fn.join(vim.fn.getline(pos, pos + 2)), pattern)
-  return { vim.fn.trim(match[2]), vim.fn.trim(match[3]) }
+    local match = vim.fn.matchlist(vim.fn.join(vim.fn.getline(pos, pos + 2)), pattern)
+    return vim.fn.trim(match[2]), vim.fn.trim(match[3])
+  end)
 end
 
 local function ctest()
@@ -53,9 +101,16 @@ local function ctest()
   vim.o.makeprg = makeprg
 end
 
-local function ctest_gtest(filter)
-  local old_errorformat = vim.o.errorformat
+local function ctest_gtest(suite, case)
+  vim.env.TEST_SUITE = suite or '*' -- doctest
+  vim.env.TEST_CASE = case or '*'   -- doctest
+  vim.env.GTEST_FILTER = vim.env.TEST_SUITE .. '.' .. vim.env.TEST_CASE
 
+  vim.env.GTEST_COLOR = '0'         -- causes problems in output
+  vim.env.ASAN_OPTIONS = 'coverage=1'
+  vim.env.CTEST_OUTPUT_ON_FAILURE = '1'
+
+  local old_errorformat = vim.o.errorformat
   vim.o.errorformat = ''
       .. '%.%#: %f:%l: %m' -- C/C++ assertions
       .. ',%Z[%.%#] %m'
@@ -64,15 +119,9 @@ local function ctest_gtest(filter)
       .. ',%A%f:%l: %t%*[^:]:\\ %m' -- doctest: assertions, ...
       .. ',%A%f:%l: %t%.%#'         -- gtest: assertions, ...
       .. ',%EFAIL: %f'              -- gtest segault, exceptions, ...
-
-  vim.env.GTEST_COLOR = '0'         -- causes problems in output
-  vim.env.GTEST_FILTER = filter or '*'
-  vim.env.ASAN_OPTIONS = 'coverage=1'
-  vim.env.CTEST_OUTPUT_ON_FAILURE = '1'
-
   ctest()
   vim.o.errorformat = old_errorformat
 end
 vim.keymap.set('n', '<Leader>yy', ctest_gtest)
-vim.keymap.set('n', '<Leader>yf', function() ctest_gtest(gtest_context()[1]..".*") end)
-vim.keymap.set('n', '<Leader>yt', function() ctest_gtest(vim.fn.join(gtest_context(), ".")) end)
+vim.keymap.set('n', '<Leader>yf', function() ctest_gtest(select(1, gtest_context()), '*') end)
+vim.keymap.set('n', '<Leader>yt', function() ctest_gtest(gtest_context()) end)
