912c578c6d6a4874f9ae6abb34a8dc6ffa9bcacb
Johannes Larsen <mail@johslarsen.net>
2024-02-25 03:21:08 +0100

    Add nvim makeprgs for markdown and asciidoctor

--- config/nvim/lua/make.lua
+++ config/nvim/lua/make.lua
@@ -7,3 +7,27 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     end
   end
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.api.nvim_create_user_command('Pandoc', function(opts)
+      local src = vim.env.DOCROOT or vim.api.nvim_buf_get_name(0)
+      local type = opts.fargs[1]
+      local dest = opts.fargs[2] ~= nil and opts.fargs[2] or src:gsub('[.][^.]*$', '.' .. type)
+      if dest == '+' then dest = '>(xclip -selection clipboard)' end
+
+      vim.cmd.Dispatch('pandoc -t ' .. type .. ' -o ' .. dest .. ' ' .. src)
+    end, { nargs = '+' })
+    vim.keymap.set('n', '<Leader>m', '<cmd>Pandoc html<CR>', { buffer = 0 })
+    vim.keymap.set('n', '<Leader>M', ':Pandoc ', { buffer = 0 })
+    vim.keymap.set('n', '<Leader><C-m>', '<cmd>Pandoc jira +<CR>', { buffer = 0 })
+  end
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "asciidoctor" },
+  callback = function()
+    vim.bo.makeprg = 'asciidoctor ' .. (vim.env.DOCROOT or vim.api.nvim_buf_get_name(0))
+  end
+})
