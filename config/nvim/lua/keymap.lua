0d357006297f128f8819a06007b1c53c343d1bac
Johannes Larsen <mail@johslarsen.net>
2024-02-26 02:18:51 +0100

    Add nvim hotkeys {Q, ..., }L to step through diagnostics in the preview window

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -50,6 +50,7 @@ vim.keymap.set('n', '<Leader><C-p>', ":lhelpgrep ")
 vim.keymap.set('v', '<Leader><C-p>', function() vim.cmd("lhelpgrep " .. fzf.utils.get_visual_selection()) end)
 vim.keymap.set('n', '<Leader>q', "<cmd>copen<CR>")
 vim.keymap.set('n', '<Leader>Q', "<cmd>cclose<CR><cmd>clist<CR>")
+vim.keymap.set('n', '<Leader><C-q>', "<cmd>cclose<CR>")
 vim.keymap.set('n', '<Leader>r', ':%s/\\<<C-r><C-w>\\>//g<Left><Left>')
 vim.keymap.set('v', '<Leader>r', '"vy:%s/<C-r>v//g<Left><Left>')
 vim.keymap.set('n', '<Leader>R', ':%s/\\<<C-r><C-a>\\>//g<Left><Left>')
@@ -62,6 +63,7 @@ vim.keymap.set('n', '<Leader>v', fzf.git_commits)
 vim.keymap.set('n', '<Leader>V', fzf.git_bcommits)
 vim.keymap.set('n', '<Leader>w', "<cmd>lopen 4<CR>")
 vim.keymap.set('n', '<Leader>W', "<cmd>lclose<CR><cmd>llist<CR>")
+vim.keymap.set('n', '<Leader><C-w>', "<cmd>lclose<CR>")
 vim.keymap.set('n', '<Leader>7', function() vim.opt.colorcolumn = vim.o.colorcolumn == "73" and "-1" or "73" end)
 vim.keymap.set('n', '<Leader>8', function() vim.opt.colorcolumn = vim.o.colorcolumn == "81" and "-1" or "81" end)
 vim.keymap.set('n', '<Leader>%', '%<C-space>', { remap = true })
@@ -93,10 +95,8 @@ local function preview_window()
   end
   return nil
 end
-local function pedit_qf_entry()
-  local getqflist = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].loclist == 1 and vim.fn.getloclist or
-  vim.fn.getqflist
-  local items = getqflist({ idx = vim.fn.line('.'), items = 1 }).items[1]
+local function pedit_qf_entry(getqflist, idx)
+  local items = getqflist({ idx = idx, items = 1 }).items[1]
   local preview = preview_window()
   if preview ~= nil and vim.api.nvim_win_get_buf(preview) == items.bufnr then -- do not reload it unnecessarily
     vim.api.nvim_win_set_cursor(preview, { items.lnum, items.col })
@@ -104,13 +104,33 @@ local function pedit_qf_entry()
     vim.cmd("pedit +:call\\ cursor(" .. items.lnum .. "," .. items.col .. ") " .. vim.api.nvim_buf_get_name(items.bufnr))
   end
 end
+local function pedit_focused_qf_entry()
+  local loclist = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].loclist == 1
+  pedit_qf_entry(loclist and vim.fn.getloclist or vim.fn.getqflist, vim.fn.line('.'))
+end
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = "qf",
   callback = function()
     vim.keymap.set('n', '<CR>', '<CR>') -- use as regular in quickfix
-    vim.keymap.set('n', '<Space>', pedit_qf_entry)
+    vim.keymap.set('n', '<Space>', pedit_focused_qf_entry)
   end,
 })
+local function preview_diagnostic(getqflist, step, reset)
+  local preview = preview_window()
+  if preview == nil then
+    pedit_qf_entry(getqflist, getqflist({ idx = 0 }).idx)
+    preview = preview_window()
+    if preview == nil then return end -- just to satisfy lsp-linter
+  end
+  if not select(1, pcall(vim.api.nvim_win_call, preview, step)) then
+    vim.api.nvim_win_call(preview, reset)
+  end
+  pedit_qf_entry(getqflist, getqflist({ idx = 0 }).idx)
+end
+vim.keymap.set('n', '{Q', function() preview_diagnostic(vim.fn.getqflist, vim.cmd.cprev, vim.cmd.clast) end)
+vim.keymap.set('n', '}Q', function() preview_diagnostic(vim.fn.getqflist, vim.cmd.cnext, vim.cmd.cfirst) end)
+vim.keymap.set('n', '{L', function() preview_diagnostic(vim.fn.getloclist, vim.cmd.lprev, vim.cmd.llast) end)
+vim.keymap.set('n', '}L', function() preview_diagnostic(vim.fn.getloclist, vim.cmd.lnext, vim.cmd.lfirst) end)
 
 vim.keymap.set('n', 'Y', 'y$') -- to be consistent with D/C
 
