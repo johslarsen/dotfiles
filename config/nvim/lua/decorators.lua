a7aea565cf3cca491e35cf7ff70682a48b26855c
Johannes Larsen <mail@johslarsen.net>
2024-12-12 00:59:27 +0100

    Fix namespaced class names in snippets

--- config/nvim/lua/decorators.lua
+++ config/nvim/lua/decorators.lua
@@ -14,10 +14,10 @@ M.qualified_class_from_file = function()
   return decorator_from_path(vim.fn.expand("%:r"))
 end
 M.class_from_line = function()
-  local pattern = '^\\s*\\(class\\|struct\\) *\\([^ ]\\+\\).*'
+  local pattern = '^\\s*\\(class\\|struct\\)\\(.*::\\)\\? *\\([^ ]\\+\\).*'
   local pos = vim.fn.search(pattern, 'bcnW')
   if (pos == 0) then return "" end
-  return vim.fn.substitute(vim.fn.getline(pos), pattern, "\\2", "")
+  return vim.fn.substitute(vim.fn.getline(pos), pattern, "\\3", "")
 end
 
 return M
