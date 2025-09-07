434ef4e5bc5a4de90d8e08fe1f37ca0351bb8eb7
Johannes Larsen <mail@johslarsen.net>
2025-09-08 00:17:12 +0200

    Fallback ranger preview to XXD and tweak HTML preview

--- config/ranger/scope.sh
+++ config/ranger/scope.sh
@@ -19,6 +19,7 @@ case "${FILE_EXTENSION,,}" in
   doc|docx) doctotxt < "${FILE_PATH}" && exit 5;;
   gpg) gpg --batch --pinentry-mode loopback --list-packets "${FILE_PATH}" && exit 5;;
   ini) cat "${FILE_PATH}" && exit 5;;
+  htm|html|xhtml) elinks -dump "${FILE_PATH}" -dump-width $PV_WIDTH -dump-color-mode 3 && exit 5;;
   pyc) strings "${FILE_PATH}" && exit 5;;
   xlsx) xlsx2csv -i "${FILE_PATH}" && exit 5;;
   csg|scad) openscad_png "$FILE_PATH" && exit 6;;
@@ -29,12 +30,15 @@ MIMETYPE="$( file --dereference --brief --mime-type -- "${FILE_PATH}" )"
 case "$MIMETYPE" in
   video/*) [[ "$PV_IMAGE_ENABLED" ]] && ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6;;
   *application|*object) readelf -hnsW "${FILE_PATH}" && exit 5;;
-  *gzip) zcat "${FILE_PATH}" | head -${PV_HEIGHT} && exit 3;;
+  *gzip) zcat "${FILE_PATH}" | head -${PV_HEIGHT} && exit 2;;
   *pcap) tcpdump -ttttnnr "${FILE_PATH}" | head -${PV_HEIGHT} && exit 3;;
   *sqlite3) sqlite3 -readonly "${FILE_PATH}" ".schema" && exit 0;;
   application/json) jq --color-output . < "${FILE_PATH}" && exit 5;;
-  application/octet-stream) xxd "${FILE_PATH}" | head -${PV_HEIGHT} && exit 3;;
 esac
 
-. /usr/share/doc/ranger/config/scope.sh # arch
-. /usr/lib/python3.6/site-packages/ranger/data/scope.sh # OpenSUSE 15.5
+override_fallback() {
+    sed 's/\(File Type Classification.*\) && exit.*/\1 \&\& xxd -R always "\${FILE_PATH}" | head -$((PV_HEIGHT-2)); exit 5/g' "$1"
+}
+
+. <(override_fallback /usr/share/doc/ranger/config/scope.sh) # arch
+. <(override_fallback /usr/lib/python3.6/site-packages/ranger/data/scope.sh) # OpenSUSE 15.5
